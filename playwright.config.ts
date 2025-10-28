f7c309a61263a3e34987704ca67ef7ad3905b1ab
hatzumy <tellezmaleja@gmail.com>
2025-10-27 21:15:27 -0500

    chore: Add screenshot function on playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
     headless: true,
+    screenshot: 'only-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
